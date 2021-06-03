cf1d92b020f7dfbe7e801ec6fcd544a0bbf8a8e9
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-03 06:57:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -38,7 +38,7 @@ def game_hash
               :rebounds => 11,
               :assists =>6 ,
               :steals =>3 ,
-              :blocks => ,
+              :blocks =>8 ,
               :slam_dunks => },
               {:player_name => ,
               :number => ,
