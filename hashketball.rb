d19c1b815aadedb5f0b6d658d1906a4162d705cd
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-03 06:52:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,19 @@
 # Write your code here!
 
 def game_hash
-  {:home => {:team_name => ,
-            :colors => ,
-            :players => }
+  {:home => {:team_name => "Brooklyn Nets" ,
+            :colors => ["Black", "White"],
+            :players => [
+              {:player_name => ,
+              :number => ,
+              :shoe => ,
+              :points => ,
+              :rebounds => ,
+              :assists => ,
+              :steals => ,
+              :blocks => ,
+              :slam_dunks => }
+              ]}
   
   , 
   :away => {:team_name => ,
