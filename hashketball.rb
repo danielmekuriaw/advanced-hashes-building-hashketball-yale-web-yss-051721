482f8b1ba53072cfa51e98e7ea10030cd99db872
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-03 07:01:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -81,7 +81,7 @@ def game_hash
               :steals => 4,
               :blocks => 5,
               :slam_dunks => 5},
-              {:player_name => ,
+              {:player_name => "Ben Gordon",
               :number => ,
               :shoe => ,
               :points => ,
