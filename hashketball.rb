daa6a0949c1393828b8f3f604a314b447f44bba1
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-03 06:49:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,11 @@
 # Write your code here!
+
+def game_hash
+  {:home => {:team_name => ,
+            :colors => ,
+            :players => }
+  
+  , 
+  :away => {:team_name => ,
+            :colors => ,
+            :players =>}
