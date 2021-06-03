dd2f2f81fa218499c1ff2a8273a28c78b2c931de
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-03 06:59:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,7 +64,7 @@ def game_hash
               :blocks => 7,
               :slam_dunks => 2},
               {:player_name => "Bismack Biyombo" ,
-              :number => ,
+              :number => 0,
               :shoe => ,
               :points => ,
               :rebounds => ,
