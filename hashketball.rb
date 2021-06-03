c99388af3375056744421173546ba2258a4dcc8d
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-03 06:53:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
             :colors => ["Black", "White"],
             :players => [
               {:player_name => "Alan Anderson",
-              :number => ,
+              :number => 0,
               :shoe => ,
               :points => ,
               :rebounds => ,
