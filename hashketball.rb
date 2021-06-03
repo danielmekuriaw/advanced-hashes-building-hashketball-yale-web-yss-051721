c2c5b5df5b43891762de7b05cc2eb27843efdb8d
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-03 07:01:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -90,7 +90,7 @@ def game_hash
               :steals => 1,
               :blocks => 1,
               :slam_dunks => 0},
-              {:player_name => ,
+              {:player_name => "Kemba Walker" ,
               :number => ,
               :shoe => ,
               :points => ,
